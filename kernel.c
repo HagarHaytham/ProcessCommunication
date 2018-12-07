549bcfd7910e0d403fad6c0afb799a0d48149811
Hagar Haytham <hagar.bahy97@eng-st.cu.edu.eg>
2018-12-07 23:33:03 +0200

    project layout

---
+++ kernel.c
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <signal.h>
+
+struct msgbuff
+{
+   long mtype; // must be long
+   char mtext[64];
+};
+
+
+int main()
+{
+
+	return 0;
+}
