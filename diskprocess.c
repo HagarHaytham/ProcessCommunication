55c832b89f365da81f50112b5d6a72319302deaf
Hagar Haytham <hagar.bahy97@eng-st.cu.edu.eg>
2018-12-17 07:21:54 +0200

    still working on disc process

--- diskprocess.c
+++ diskprocess.c
@@ -10,20 +10,35 @@
 #include <ctype.h>
 #include <unistd.h>
 #include <signal.h>
+#include <stdbool.h> 
+#define NoOfSlots 10
 
 struct msgbuff
 {
 	long mtype; // must be long
 	char mtext[64];
 };
-
-int clk =0;
-int diskStatus =-1;
+//const int NoOfSlots=10;// this is not really a const in c ,, can't use it 
+int prev_clk=-1,clk=-1;
+int diskStatus =6;// disk status 
+// succ add 0
+// succ del 1
+// add failed 2
+//deletion failed 3
+// Adding takes 3 clks?
+//deletion takes 1 clk?
+// adding 4
+// deleting 5
+// nothing 6
 int freeSlots=10;
 key_t upMsgqId,downMsgqId;
+bool slotfull[NoOfSlots];
+char Slots [NoOfSlots][64];
+
 
 void IncClk(int signum) // Handler for SIGUSR2
 {
+	prev_clk=clk;
 	clk++;
 }
 
@@ -66,19 +81,65 @@ void RecieveMsg()
 
 }
 
+void Add(char * msg)
+{
+	// searches for an empty slot and adds the msg in it
+	// takes 3 secs
+	int begin =clk; // begin now
+	diskStatus =4;//adding
+	int i=0;
+	while (slotfull[i] && i<NoOfSlots) // false :empty , true :full
+		i++;
+	while (begin +3 >clk)
+		continue;// just wait 3 seconds to add
+	if (i==NoOfSlots) // aw el msg akbr mn 64 ??????????
+		diskStatus=2;// failed to add
+	else
+	{
+		strcpy(Slots[i],msg);
+		diskStatus=0;// successful add
+	}
+
+
+}
+void Delete(int slotno)
+{
+	// deletes the msg in the slot no given
+	//takes 1 sec
+	int begin = clk;
+	diskStatus= 5;//deleting
+	while (begin +1 >clk)
+		continue;// just wait 1 seconds to delete
+	if (slotno<NoOfSlots && slotfull[slotno])
+	{
+		//delete the char *
+		diskStatus=1; // successful delete
+	}
+	else
+		diskStatus=3; // failed to delete
+}
+
 int main()
 {
 	signal(SIGUSR2,IncClk);
 	signal(SIGUSR1,SendMsg);
+	memset(slotfull ,0, sizeof(slotfull[0]) * NoOfSlots); // all slots are empty at the begining
+	// kernel should wait until both processes are running 
+	// so an idea to handle this is to let te disk process create the msg queues 
+	//and the kernel waits until they are created , same with the Process process
+	// we can also send a signal maybe ?
 	upMsgqId = msgget(99, IPC_CREAT|0644);//99 for up // dummy values 
-	downMsgqId = msgget(100, IPC_CREAT|0644);// 100 for down
+	downMsgqId = msgget(100, IPC_CREAT|0644);// 100 for down // kernel should have the same keys
 	if(upMsgqId == -1 ||downMsgqId == -1 )
 	{	
 		perror("Error in create");
 		exit(-1);
 	}
-	char Slots [10][64];
-
+	prev_clk=-1;
+	clk=-1;
+	// so for syncronization we have to work with the same clk given 
+	while (clk==prev_clk)// -1 at first , the disk process shouldn't start now
+		continue;
 
 
 
