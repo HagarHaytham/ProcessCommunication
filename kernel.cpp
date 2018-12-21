9d1bcb10ed1a715f2ccefb934f98f34500945ce0
rehamhy6 <rihamhamdy6@gmail.com>
2018-12-21 23:41:30 +0200

    updated kernel functions now it can communicate with disk and process , note : update message struct to contain variable 'char request type' which can hold int data types

--- kernel.cpp
+++ kernel.cpp
@@ -12,6 +12,7 @@
 #include <signal.h>
 #include <vector>
 #include <string>
+using namespace std;
 const int disk_type =0;
 const int process_type=1;
 const int disk_up=99;
@@ -20,7 +21,7 @@ const int process_up=199;
 const int process_down=200;
 int disk_id;
 int process_counter=0;
-vector <string> log;
+vector <string> message_log;
 vector <int> process_list;
 int disk_up_queue = msgget(disk_up, IPC_CREAT|0644);//99 for up // dummy values 
 int disk_down_queue = msgget(disk_down, IPC_CREAT|0644);// 100 for down // kernel should have the same keys
@@ -33,26 +34,26 @@ struct msgbuff
    char mtext[64];
 };
 
-void initialize(disk_up_queue,process_up_queue)
+void initialize(int disk_up_queue,int process_up_queue)
 {
-	process_Counter=0;
 	struct msgbuff first_message;
-        recieve = msgrcv(disk_up_queue, &first_message, sizeof(first_message.mtext),0, !IPC_NOWAIT);   // receive on up , send on down
+        int recieve = msgrcv(disk_up_queue, &first_message, sizeof(first_message.mtext),0, !IPC_NOWAIT);   // receive on up , send on down
       	disk_id=first_message.mtype;
 	recieve = msgrcv(process_up_queue, &first_message, sizeof(first_message.mtext),0, !IPC_NOWAIT); // receive on up , send on down   
-	process_list[process_counter++]=first_message.mtype;
+	process_list.push_back(first_message.mtype);
 	
 }
 int disk_status()
 {
 struct msgbuff message;
-recieve = msgrcv(disk_up_queue, &message, sizeof(message.mtext),0, !IPC_NOWAIT); 
-if(recieve != -1 && message.mtype=disk_id )
-return (int)message.mtext;
+int recieve = msgrcv(disk_up_queue, &message, sizeof(message.mtext),0, !IPC_NOWAIT); 
+if(recieve != -1 && message.mtype==disk_id )
+return message.request_type;
 return -1;
 }
 int process_request(struct msgbuff message)
 {
+int latency=0;
 if(message.request_type == 'I')
 {
 	process_list[process_counter++]=(int)message.mtype;
@@ -61,7 +62,7 @@ if(message.request_type == 'I')
 else
 { 
 	struct msgbuff kernel_response;
-	int latency=0;
+
 	killpg(disk_id,SIGUSR1);
 	int disk_response=disk_status();
 
@@ -70,21 +71,24 @@ else
 
 	if(message.request_type == 'D' && disk_response >0)
 	{
-		message.mtext=1;
-		letency=1;
+		kernel_response.request_type=1;
+		int send = msgsnd(disk_down_queue, &kernel_response, sizeof(message.mtext), IPC_NOWAIT);
+		latency=1;
 	}
-	else 	message.mtext=3;
+	else 	kernel_response.request_type=3;
 	if(message.request_type == 'A' && disk_response <10)
 	{
-		message.mtext=0;
-		letency=3;
+		kernel_response.request_type=0;
+		int send = msgsnd(disk_down_queue, &kernel_response, sizeof(message.mtext), IPC_NOWAIT);
+		latency=3;
 	}
-	else 	message.mtext=2;
+	else 	kernel_response.request_type=2;
 	kernel_response.mtype=message.mtype;
-	send = msgsnd(upMsgqId, &message, sizeof(message.mtext), IPC_NOWAIT);
+	int send = msgsnd(process_down_queue, &kernel_response, sizeof(message.mtext), IPC_NOWAIT);
 	}
 
 }
+return 	latency;
 }
 
 
@@ -97,15 +101,15 @@ int main()
 	int clk=0;
 	while(1)
 	{
-	for(int i=0;i<process_id.size();i++)
+	for(int i=0;i<process_list.size();i++)
 	killpg(process_list[i],SIGUSR2);
 	
 	struct msgbuff message;
 	int latency=0;
-	recieve = msgrcv(process_up_queue, &message, sizeof(message.mtext),0, IPC_NOWAIT);  
+	int recieve = msgrcv(process_up_queue, &message, sizeof(message.mtext),0, IPC_NOWAIT);  
 	if(recieve !=-1 )
 	{
-	log.push_back(message.request_type+" "+message.mtext);
+	message_log.push_back(message.request_type+" "+(string)message.mtext);
 	
 	latency=process_request(message);
 	}
